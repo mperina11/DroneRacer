7a348658a88ec23d7d4eb2d45c8d02a2f362f6f1
mperina11 <mperina173@gmail.com>
2022-07-21 15:04:44 -0700

    single player added

--- main.js
+++ main.js
@@ -5,15 +5,15 @@ const canvasH = 600;
 // Gates sizes
 const gateW = 30;
 const gateH = 75;
-const gateOffsetX = gateW/2;
-const gateOffsetY = gateH/2;
+const gateOffsetX = gateW / 2;
+const gateOffsetY = gateH / 2;
 
 // seed
 let seed = 123456;
 
 // Colors
-const mix =  ["#9B5DE5","#F15BB5","#FEE440","#00BBF9","#00F5D4", "#FFBE0B","#FB5607","#FF006E","#8338EC","#3A86FF"];
-const rainbow = ["#FF0000","#FF8700","#FFD300","#DEFF0A","#A1FF0A","#0AFF99","#0AEFFF","#147DF5","#580AFF","#BE0AFF"];
+const mix = ["#9B5DE5", "#F15BB5", "#FEE440", "#00BBF9", "#00F5D4", "#FFBE0B", "#FB5607", "#FF006E", "#8338EC", "#3A86FF"];
+const rainbow = ["#FF0000", "#FF8700", "#FFD300", "#DEFF0A", "#A1FF0A", "#0AFF99", "#0AEFFF", "#147DF5", "#580AFF", "#BE0AFF"];
 let gate_color;
 
 // Debug Flag
@@ -24,8 +24,8 @@ let Points = [];
 let gateEdges = [];
 
 // Laps
-let Finish = 1;
-let finished = false;   
+let Finish = 3;
+let finished = false;
 let Done = false;
 
 // Drones
@@ -52,19 +52,22 @@ let single_player = false;
 // date
 let date_start;
 let date_end;
+let timer = 0;
 
 function setup() {
   createCanvas(canvasW, canvasH);
+  createButton("Single Player").mousePressed(()=> { single_player = true; });
+  createButton("AI Race").mousePressed(()=> { single_player = false; });
   createButton("Reroll").mousePressed(() => { seed++; createPoints_Gates(); });
-  createButton("Toggle Debug").mousePressed(() => { if (debug) {debug=false;} else {debug=true;} });
+  createButton("Toggle Debug").mousePressed(() => { if (debug) { debug = false; } else { debug = true; } });
   createP(" 'n' to Start   |   Fly through each gate, track progress in counters");
   createP(" 'm' to Pause   |   'WASD' to move your 'White' drone");
 
   createPoints_Gates();
 
   swarm = new Swarm();
-  for (let i=1; i < 5; i++) {
-    let d = new Drone(10, 100 + 25*i);
+  for (let i = 1; i < 5; i++) {
+    let d = new Drone(10, 100 + 25 * i);
     swarm.addDrone(d);
     // console.log("D: ", d);
   }
@@ -86,68 +89,101 @@ function draw() {
     // debug_drawPointLines();
     debug_drawRectPoints();
   }
-  
+
   // Draw Track
   createGates();
   createTrackCornersLeft();
   createTrackCornersRight();
   createTrackLines();
 
-  // Start AI Race
-  Start();
-  Pause();
-  swarm.run();
-  player_run();
-  playerCheckGate();
 
-  // Display info
-  let display_gate = player_current_gate;
-  if (player_current_gate == 0) {
-    display_gate = 8;
+  if (single_player) { // Start Single Player Race
+    Start();
+    Pause();
+
+    player_run();
+    playerCheckGate();
+
+     // Display info
+     let display_gate = player_current_gate;
+     if (player_current_gate == 0) {
+       display_gate = 8;
+     }
+     textSize(20);
+     text("Current Lap = " + player_lap + " | Race Finished = " + finished + " | Time = " + timer +
+       "\nGates Passed = " + display_gate, 10, 10, width / 2, height / 2);
+ 
+     // Display Gate #'s
+     for (let i = 0; i < Points.length; i++) {
+       fill('black');
+       text(i + 1, Points[i].x - 5, Points[i].y - 5, width / 2, height / 2);
+     }
+ 
+     // Check for Finish
+     if (player_lap == Finish && !Done) {
+       pause = true;
+       player_pause = true;
+       finished = true;
+       Done = true;
+       date_end = Date.now();
+       console.log("Date End: ", date_end);
+       timer = date_end - date_start;
+       console.log("Date Dif: ", (timer) / 1000);
+     }
   }
-  textSize(20);
-  text("Current Lap = " + player_lap + " | Race Finished = " + finished +
-       "\nGates Passed = " + display_gate, 10, 10, width/2, height/2);  
+  else { // Start AI Race
 
-  // Display Gate #'s
-  for (let i=0; i < Points.length; i++) {
-    fill('black');
-    text(i+1, Points[i].x - 5, Points[i].y - 5, width/2, height/2);
-  }
+    Start();
+    Pause();
+    swarm.run();
+    player_run();
+    playerCheckGate();
 
-  // Check for Finish
-  if (player_lap == Finish && !Done) {
-    pause = true;
-    player_pause = true;
-    finished = true;
-    Done = true;
-    date_end = Date.now();
-    console.log("Date End: ", date_end);
-    console.log("Date Dif: ", (date_end - date_start) / 1000);
-  }
+    // Display info
+    let display_gate = player_current_gate;
+    if (player_current_gate == 0) {
+      display_gate = 8;
+    }
+    textSize(20);
+    text("Current Lap = " + player_lap + " | Race Finished = " + finished +
+      "\nGates Passed = " + display_gate, 10, 10, width / 2, height / 2);
 
-  if (single_player) {
+    // Display Gate #'s
+    for (let i = 0; i < Points.length; i++) {
+      fill('black');
+      text(i + 1, Points[i].x - 5, Points[i].y - 5, width / 2, height / 2);
+    }
 
+    // Check for Finish
+    if (player_lap == Finish && !Done) {
+      pause = true;
+      player_pause = true;
+      finished = true;
+      Done = true;
+      date_end = Date.now();
+      console.log("Date End: ", date_end);
+      console.log("Date Dif: ", (date_end - date_start) / 1000);
+    }
   }
 
 }
 
 function Start() { // start
   if (key == 'n') {
-    pause = false; 
-    player_pause = false; 
+    pause = false;
+    player_pause = false;
   }
 }
 
 function Pause() { // stop
   if (key == 'm') {
-    pause = true; 
+    pause = true;
     player_pause = true;
   }
 }
 
 function player_run() {
-  
+
   Type_B(xpos, ypos, player_color);
 
   if (!player_pause) {
@@ -163,7 +199,7 @@ function player_run() {
     if (movingDown) {
       ypos += player_speed;
     }
-}
+  }
 }
 
 
@@ -208,23 +244,28 @@ function player_atGate() {
     return true;
   }
 
-	return false;
+  return false;
 }
 
 function playerCheckGate() {
   if (player_atGate()) {
 
-		// count lap
-		if (player_current_gate == 0 && player_last_gate_passed) {
-			player_lap++;
-			console.log("PL: ", player_lap);
-		}
-		// next gate
-		player_current_gate++;
-		// check last gate 
-		if (player_current_gate == 8) {
-			player_current_gate = 0; // back to start
-			player_last_gate_passed = true;
-		}
-	}
+    // count lap
+    if (player_current_gate == 0 && player_last_gate_passed) {
+      player_lap++;
+      console.log("PL: ", player_lap);
+    }
+    // next gate
+    player_current_gate++;
+    // check last gate 
+    if (player_current_gate == 8) {
+      player_current_gate = 0; // back to start
+      player_last_gate_passed = true;
+      
+      if (single_player) {
+        seed++;
+        createPoints_Gates();
+      }
+    }
+  }
 }
